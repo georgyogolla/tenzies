ea2a0c1ee7caaa0fcae4c0b2e84d8b19c11725ca
george ogolla <georgyogolla@gmail.com>
2023-04-28 21:18:27 +0300

    toggling unmarked dices

--- src/App.jsx
+++ src/App.jsx
@@ -1,27 +1,61 @@
 import { useState } from "react";
 import "./App.css";
 import Die from "./Die";
+import { nanoid } from "nanoid";
 
 function App() {
   const [dice, setDice] = useState(allNewDice());
 
+  // Helper function to genrate new dice
+  function generateNewDie() {
+    return {
+      value: Math.ceil(Math.random() * 6),
+      isHeld: false,
+      id: nanoid(),
+    };
+  }
   function allNewDice() {
     const newDice = [];
     for (let i = 0; i < 10; i++) {
-      newDice.push(Math.ceil(Math.floor(Math.random() * 6)));
+      newDice.push(generateNewDie());
     }
     return newDice;
   }
-  // console.log(allNewDice());
 
-  function rollDice(){
-    setDice(allNewDice());
+  // Updating the roll dice function to not just roll all new dice, but instead to look through the existing dice to not roll any that are being held
+
+  function rollDice() {
+    setDice((oldDice) =>
+      oldDice.map((die) => {
+        return die.isHeld ? die : generateNewDie();
+      })
+    );
   }
-  const diceElements = dice.map(die => <Die value={die}/>)
+
+  function holdDice(id) {
+    setDice((oldDice) =>
+      oldDice.map((die) => {
+        return die.id === id ? { ...die, isHeld: !die.isHeld } : die;
+      })
+    );
+  }
+
+  const diceElements = dice.map((die) => (
+    <Die
+      key={die.id}
+      value={die.value}
+      isHeld={die.isHeld}
+      holdDice={() => holdDice(die.id)}
+    />
+  ));
   return (
     <main>
+      <h1 className="title">Tenzies</h1>
+      <p className="instructions">Roll until dice are the same. Click each die to freeze it at its current value between rolls.</p>
       <div className="dice-container">{diceElements}</div>
-      <button className="roll-dice" onClick={rollDice}>Roll</button>
+      <button className="roll-dice" onClick={rollDice}>
+        Roll
+      </button>
     </main>
   );
 }
